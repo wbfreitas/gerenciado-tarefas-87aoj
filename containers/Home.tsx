d59194c6213bdf2640e5fd1677650da1362e3190
Wellington Barros Freitas <wfreitas@Wellingtons-MacBook-Pro.local>
2022-09-02 21:01:45 -0300

    antes do codigo do professo

--- containers/Home.tsx
+++ containers/Home.tsx
@@ -7,14 +7,33 @@ import { List } from "../components/List";
 import { useEffect, useState } from "react";
 import { executeRequest } from "../services/apiServices";
 import { Task } from "../types/Task";
+import { Modal } from "react-bootstrap";
 
 export const Home : NextPage<AccessTokenProps> = ({setAccessToken}) => {
 
     const [tasks, setTasks] = useState<Task[]>([]);
+    const [previsionDateStart, setPrevisionDateStart ] = useState('');
+    const [previsionDateEnd, setPrevisionDateEnd ] = useState('');
+    const [status, setStatus ] = useState('0');
+
+    const [showModal, setShowModal ] = useState(false);
+    const [error, setError] = useState('');
+    const [name, setName] = useState('');
+    const [modalPrevisionDateStart, setModalPrevisionDateStart] = useState('');
+    const [modalPrevisionDateEnd, setModalPrevisionDateEnd] = useState('');
 
     const getFilteredList = async() =>{
         try{
-            const result = await executeRequest('task', 'GET');
+
+            let query = '?status=' + status;
+
+            if(previsionDateStart) 
+                    query += '&previsionDateStart=' + previsionDateStart;
+            
+            if(previsionDateEnd) 
+                query += '&previsionDateEnd=' + previsionDateEnd;
+
+            const result = await executeRequest('task'+ query, 'GET');
             if(result && result.data){
                 setTasks(result.data);
             }
@@ -23,9 +42,38 @@ export const Home : NextPage<AccessTokenProps> = ({setAccessToken}) => {
         }
     }
 
+    const salvar = async() => {
+        console.log(!name);
+        console.log(modalPrevisionDateStart);
+        console.log(!name.trim());
+        console.log(!modalPrevisionDateStart.trim());
+        
+        if(!name || !name.trim() ||  !modalPrevisionDateStart || !modalPrevisionDateStart.trim() ) {
+            setError('Favor preencher o formulario');
+            return;
+        }
+        const body = {name, previsionDate: previsionDateStart}   
+        try {
+            await executeRequest('task', 'POST', body);
+            await getFilteredList();
+            closeModal();
+        } catch (e: any) {
+           if(e?.response?.data?.error) {
+            setError(e?.response?.data?.error);
+           } 
+        }
+    }
+    const closeModal = () => {
+        setName('');
+        setError('');
+        setModalPrevisionDateStart('');
+        setModalPrevisionDateEnd('');
+    }
+
     useEffect(() =>{
         getFilteredList();
-    }, []);
+    }, [previsionDateStart, previsionDateEnd, status, name]);
+
 
     const sair = () =>{
         localStorage.removeItem('accessToken');
@@ -36,10 +84,39 @@ export const Home : NextPage<AccessTokenProps> = ({setAccessToken}) => {
 
     return (
         <>
-            <Headers sair={sair}/>
-            <Filter/>
+            <Headers sair={sair} setShowModal={setShowModal}/>
+            <Filter periodoDe={previsionDateStart} setPeriodoDe={setPrevisionDateStart}
+                setPeriodoAte={setPrevisionDateEnd}
+                periodoAte={previsionDateEnd} status={status} setStatus={setStatus} />
             <List tasks={tasks} getFilteredList={getFilteredList} />
-            <Footer />
+            <Footer setShowModal={setShowModal} />
+            <Modal show={showModal} onHide={closeModal} className="container-modal">
+                <Modal.Body>
+                    <p>Adicionar Tarefa</p>
+                    {error && <p className='error'>{error}</p>}
+
+                    <input placeholder="nome da tarefa"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    ></input>
+
+                    <input placeholder="digite uma tarefa"
+                    type="text"
+                    onFocus={e => e.target.type =  "date"}
+                    onBlur={e => modalPrevisionDateStart ? e.target.type = 'date' : 'text'}
+                    value={modalPrevisionDateStart}
+                    onChange={e => setModalPrevisionDateStart(e.target.value)}
+                    ></input>
+                </Modal.Body>
+                <Modal.Footer>
+                    <div className="button col-12">
+
+                    <button onClick={salvar}>enviar</button>
+                    <span onClick={closeModal}>cancelar</span>
+
+                    </div>
+                </Modal.Footer>
+            </Modal>
         </>
     );
 }
